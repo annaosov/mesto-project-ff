0e5a9438e60bd4b3a68c95fe551325e79d391464
Anna O <annaosovskaya@yandex.ru>
2023-12-16 16:40:10 +0300

    добавлено удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -13,11 +13,21 @@ const places = document.querySelector('.places__list');
 function showCard(card) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
     
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__title').textContent = card.name;
 
-    places.append(cardElement); 
+    places.append(cardElement);
+
+    deleteButton.addEventListener('click', function () {
+        const listItem = deleteButton.closest('.card');
+        deleteCard(listItem);
+    });
+}
+
+function deleteCard(card) {
+    card.remove();
 }
 
 initialCards.forEach(item => {
