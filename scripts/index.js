25e9658f26e1843bfffe90e5f7210447ecb744d7
Anna O <annaosovskaya@yandex.ru>
2023-12-16 16:09:57 +0300

    добавлены карточки

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,19 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const places = document.querySelector('.places__list');
+
+function showCard(card) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    places.append(cardElement); 
+}
+
+initialCards.forEach(item => {
+    showCard(item);
+})
