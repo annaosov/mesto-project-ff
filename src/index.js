df75665f3c835a9e9b818209f373a829c756a095
Anna O <annaosovskaya@yandex.ru>
2024-01-11 21:36:03 +0300

    добавлено закрытие по крестику

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import './pages/index.css';
 import { initialCards, createCard, deleteCard } from './scripts/cards.js';
-import { openPopup } from './scripts/modals.js';
+import { openPopup, closePopup } from './scripts/modals.js';
 
 const places = document.querySelector('.places__list');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileAddButton = document.querySelector('.profile__add-button');
+const popupCloseButton = document.querySelectorAll('.popup__close');
 
 initialCards.forEach(item => {
     places.append(createCard(item, deleteCard))
-})
+});
 
 profileEditButton.addEventListener('click', function () {
     let popup = document.querySelector('.popup_type_edit');
@@ -20,3 +21,9 @@ profileAddButton.addEventListener('click', function () {
     openPopup(popup);
 });
 
+popupCloseButton.forEach(item => {
+    item.addEventListener('click', function () {
+        const popup = item.closest('.popup');
+        closePopup(popup);
+    });
+})
