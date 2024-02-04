ec2de31fd21fc7a02b082c8fac506ad0c5338da3
Anna O <annaosovskaya@yandex.ru>
2024-02-04 17:58:56 +0300

    валидация форм 1

--- src/index.js
+++ src/index.js
@@ -80,3 +80,76 @@ function handleFormNewPlaceSubmit(evt) {
 
 formNewPlace.addEventListener('submit', handleFormNewPlaceSubmit);
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input_error_active');
+  };
+  
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_error');
+    errorElement.classList.remove('popup__input_error_active');
+    errorElement.textContent = '';
+  };
+
+const isValid = (formElement, inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  };
+
+const toggleButtonState = (inputList, buttonElement) => {
+    // Если есть хотя бы один невалидный инпут
+    if (hasInvalidInput(inputList)) {
+      // сделай кнопку неактивной
+          buttonElement.disabled = true;
+      buttonElement.classList.add('popup__button_inactive');
+    } else {
+          // иначе сделай кнопку активной
+          buttonElement.disabled = false;
+      buttonElement.classList.remove('popup__button_inactive');
+    }
+  };
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(`.popup__input`));
+  const buttonElement = formElement.querySelector('.popup__button');
+
+  if (formElement.name === 'new-place') {
+    toggleButtonState(inputList, buttonElement);
+  };
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement);
+
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+    formList.forEach((formElement) => {
+      setEventListeners(formElement);
+    });
+  };
+  
+  enableValidation();
